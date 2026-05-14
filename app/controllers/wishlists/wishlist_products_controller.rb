c34019a11d371f59fc8428b002f053bcdd006844
Ton <mystvearn147@gmail.com>
2026-05-14 13:03:03 +0700

    feat: add product movement between wishlists
    Add update action to Wishlists::WishlistProductsController that moves products between user's wishlists with success/error notices. Add form to wishlist show view with collection_select for moving products to other wishlists, visible only to wishlist owners when they have multiple wishlists.

--- app/controllers/wishlists/wishlist_products_controller.rb
+++ app/controllers/wishlists/wishlist_products_controller.rb
@@ -2,6 +2,15 @@ class Wishlists::WishlistProductsController < ApplicationController
   before_action :set_wishlist
   before_action :set_wishlist_product
 
+  def update
+    new_wishlist = Current.user.wishlists.find(params[:new_wishlist_id])
+    if @wishlist_product.update(wishlist: new_wishlist)
+      redirect_to @wishlist, status: :see_other, notice: "#{@wishlist_product.product.name} has been moved to #{new_wishlist.name}"
+    else
+      redirect_to @wishlist, status: :see_other, alert: "#{@wishlist_product.product.name} is already on #{new_wishlist.name}."
+    end
+  end
+
   def destroy
     @wishlist_product.destroy
     redirect_to @wishlist, notice: "#{@wishlist_product.product.name} removed from wishlist."
